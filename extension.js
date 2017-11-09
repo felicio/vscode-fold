d0b3b54d54a7cb4d23f5678d61ffab9110d60770
Felicio Mununga <felicio@users.noreply.github.com>
2017-11-09 12:19:25 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,4 +1,18 @@
+'use strict';
+
 const vscode = require('vscode');
 
-exports.activate = () => {
-}
+exports.activate = context => {
+  const subscription = vscode.workspace.onDidOpenTextDocument(() => {
+    const maximumFoldLevel = 9;
+    const level = 1;
+
+    // FIXME: First unfold
+    vscode.commands.executeCommand(`editor.foldLevel${level}`);
+    for(let index = level + 1; index <= maximumFoldLevel; index++) { // fold recursively
+        vscode.commands.executeCommand(`editor.foldLevel${index}`);
+    }
+  });
+
+  context.subscriptions.push(subscription);
+};
