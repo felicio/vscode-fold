bc851a2abbacc1e3e328b90e9f57fecb501dab6b
Felicio Mununga <felicio@users.noreply.github.com>
2017-11-09 22:29:16 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -2,17 +2,29 @@
 
 const vscode = require('vscode');
 
-exports.activate = context => {
-  const subscription = vscode.workspace.onDidOpenTextDocument(() => {
-    const maximumFoldLevel = 9;
-    const level = 1;
+const MAX_FOLD_LEVEL = 9;
 
-    // FIXME: First unfold
-    vscode.commands.executeCommand(`editor.foldLevel${level}`);
-    for(let index = level + 1; index <= maximumFoldLevel; index++) { // fold recursively
-        vscode.commands.executeCommand(`editor.foldLevel${index}`);
-    }
-  });
+/** Activates extension on an emitted event. Invoked only once. */
+exports.activate = context => {
+  const subscription = vscode.workspace.onDidOpenTextDocument(listener);
 
   context.subscriptions.push(subscription);
 };
+
+/** Listens for text document `didOpen` event. */
+function listener() {
+  const configuration = vscode.workspace.getConfiguration('fold');
+  const level = configuration.get('level', 1);
+
+  fold(level);
+}
+
+/** Recursively folds source code regions, except the region at the current cursor position. */
+function fold(level) {
+  vscode.commands.executeCommand('editor.unfoldAll');
+  vscode.commands.executeCommand(`editor.foldLevel${level}`);
+
+  for (let index = level + 1; index <= MAX_FOLD_LEVEL; index++) {
+    vscode.commands.executeCommand(`editor.foldLevel${index}`);
+  }
+}
